4087ac294167f4e22a1273b9dea8388b99aa882b
DellChuy <rcr6797@gmail.com>
2018-02-06 21:51:02 -0800

    Add files via upload

---
+++ 18-02-06-Practica03/18-02-06-Practica03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_02_06_Practica03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData datos = new WeatherData();
+            DisplayA displayA = new DisplayA();
+            DisplayB displayB = new DisplayB();
+            datos.registrar(displayA);
+            datos.registrar(displayB);
+            datos.temperatura = 10.90;
+            datos.humedad = 500.878898;
+            datos.presion = 100.873;
+
+            datos.measurenntChanged();
+            
+           
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 18-02-06-Practica03/18-02-06-Practica03/WeatherData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_02_06_Practica03
+{
+    class WeatherData : Subject
+    {
+        public double temperatura { get; set; }
+        public double humedad { get; set; }
+        public double presion { get; set; }
+        List<WeatherObserver> observadores = new List<WeatherObserver>();
+
+        public void registrar(WeatherObserver nuevo)
+        {
+            observadores.Add(nuevo);
+            Console.WriteLine("Se registro correctamente!");
+        }
+
+        public void remover(WeatherObserver observer)
+        {
+            observadores.Remove(observer);
+        }
+
+        public void notificar()
+        {
+            Console.WriteLine("Hay Cambios!");
+            for(int i = 0; i < observadores.Count; i++)
+            {
+                observadores[i].update(temperatura,humedad,presion);
+
+            }
+        }
+        public void measurenntChanged()
+        {
+            notificar();
+        }
+    }
+}
