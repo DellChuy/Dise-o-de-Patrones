25e6af7777bb2f0d8f673726a6b19673c6eb0114
DellChuy <rcr6797@gmail.com>
2018-03-13 21:26:24 -0700

    Add files via upload

---
+++ Practica08-13-03-2018/Practica08-13-03-2018/MamaMia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica08_13_03_2018
+{
+    class MamaMia : pizzastore
+    {
+        public override pizza createpizza(string dato)
+        {
+            pizza mipizza;
+            switch (dato)
+            {
+                case "peperoni":
+                    mipizza = new peperoni();
+                    break;
+                case "especial":
+                    mipizza = new especial();
+                    break;
+                case "mexicana":
+                    mipizza = new mexicana();
+                    break;
+                default:
+                    mipizza = new peperoni();
+                    break;
+            }
+
+            mipizza.hornear();
+            mipizza.empacar();
+            mipizza.cortar();
+
+            Console.WriteLine("Se creo una pizza " + mipizza.getnommbre() + " estilo MamaMia.");
+            return mipizza;
+            
+
+        }
+    }
+}
---
+++ Practica08-13-03-2018/Practica08-13-03-2018/especial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica08_13_03_2018
+{
+    class especial : pizza
+    {
+        
+        public especial()
+        {
+            nombre = "especial";
+        }
+    }
+}
